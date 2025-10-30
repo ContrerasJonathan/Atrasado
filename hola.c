ca090c6e01d69e93cb1095753d0b0aaf09fac54a
ContrerasJonathan <L22211539@tectijuana.edu.mx>
2025-10-30 11:17:52 -0700

    Create hola.c

---
+++ hola.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // printf es una función de librería que,
+    // en el fondo, termina llamando al sistema operativo
+    // para escribir en la consola.
+    printf("Hola desde C! este es mi primer acercamiento wuuu c8\n");
+    
+    return 0; // El '0' indica que el programa terminó bien.
+}
